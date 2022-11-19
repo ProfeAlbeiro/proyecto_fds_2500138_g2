bc2c5630b392489a9a584e6ced23c274585c9ec7
ProfeAlbeiro <profealbeiro2020@gmail.com>
2022-11-19 16:00:21 -0500

    DTO - Conexión PDO - DAO

--- controllers/Users.php
+++ controllers/Users.php
@@ -1,9 +1,15 @@
 <?php
 
     require_once "models/model_dto/UserDto.php";
+    require_once "models/model_dao/UserDao.php";
 
     class Users{
-        public function __construct(){}
+        // Atributo Encapsulado
+        private $userDao;
+
+        public function __construct(){
+            $this->userDao = new UserDao;
+        }
         public function index(){            
             require_once "views/roles/admin/header.php";
             require_once "views/roles/admin/admin_main.view.php";
@@ -20,6 +26,7 @@
             }
             // Método Post
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
                 // Capturar los datos y crear Objeto DTO
                 # Por Constructor
                 $userDto = new UserDto(
@@ -28,24 +35,29 @@
                 );
                 
                 # Por Método Set('parámetro');
-                $userDto2 = new UserDto;
-                $userDto2->setCodigoRol($_POST['rol_codigo']);
-                $userDto2->setNombreRol($_POST['rol_nombre']);
+                // $userDto2 = new UserDto;
+                // $userDto2->setCodigoRol($_POST['rol_codigo']);
+                // $userDto2->setNombreRol($_POST['rol_nombre']);
+                
                 // Validar los datos
+                // ?????????
 
                 // Pasar el Objeto DTO al método (CRUD) del Objeto DAO
+                $this->userDao->createRolDao($userDto);
 
                 // Redireccionar a un controlador o una página web                
                 require_once "views/roles/admin/header.php";
                 # Objeto 1
-                echo "<br><h1>Objeto 1: Constructor</h1>";
-                echo "<br><h3>Código Rol: " . $userDto->getCodigoRol() . "</h3>";
-                echo "<br><h3>Nombre Rol: " . $userDto->getNombreRol() . "</h3>";
+                echo "Se ha creado el ROL";
+                // echo "<br><h1>Objeto 1: Constructor</h1>";
+                // echo "<br><h3>Código Rol: " . $userDto->getCodigoRol() . "</h3>";
+                // echo "<br><h3>Nombre Rol: " . $userDto->getNombreRol() . "</h3>";
 
                 # Objeto 2
-                echo "<br><h1>Objeto 2: Métodos Set</h1>";
-                echo "<br><h3>Código Rol: " . $userDto2->getCodigoRol() . "</h3>";
-                echo "<br><h3>Nombre Rol: " . $userDto2->getNombreRol() . "</h3>";
+                // echo "<br><h1>Objeto 2: Métodos Set</h1>";
+                // echo "<br><h3>Código Rol: " . $userDto2->getCodigoRol() . "</h3>";
+                // echo "<br><h3>Nombre Rol: " . $userDto2->getNombreRol() . "</h3>";
+
                 require_once "views/roles/admin/footer.php";
                 // header("Location: ?c=Dashboard");
             }
